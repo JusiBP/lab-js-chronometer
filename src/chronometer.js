e6669b5bd3a35b1d8cb303093fa59de76b54d2d5
JB <josepbardesp@gmail.com>
2022-09-20 16:10:52 +0200

    final commit

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,54 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime ++;
+      if (printTimeCallback){
+        return printTimeCallback();
+      }
+    },1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
-  }
+    let valueString = value.toString();
+    if (valueString.length <= 1) {
+      return `0${value}`;
+    }
+    else if (valueString.length > 1) {
+      return `${value}`;
+    }
+    }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
+
   }
 
   split() {
     // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
